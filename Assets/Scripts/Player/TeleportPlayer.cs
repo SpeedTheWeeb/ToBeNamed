6eed1e3ab3f9df043d53e36b0ef249a8dbcbd906
Johnny Nguyen <nguyenjohnny655@gmail.com>
2022-05-06 13:06:58 +0200

    TP to lower decks

---
+++ Assets/Scripts/Player/TeleportPlayer.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportPlayer : MonoBehaviour
+{
+    public GameObject tpPoint;
+    public GameObject Player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.name == "TPObj")
+        {
+            Debug.Log(tpPoint.transform.position);
+            Player.transform.position = tpPoint.transform.position;
+        }
+    }
+}
