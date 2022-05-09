c673545dbdc9c5b2ab9ab5105bf954b214486a67
Johnny Nguyen <nguyenjohnny655@gmail.com>
2022-05-09 11:40:06 +0200

    Implemented scene changing and changing based on actions

---
+++ Assets/Scripts/Objects/SceneChanger.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneChanger : MonoBehaviour
+{
+    Scene currentScene;
+    public GameObject robe;
+    public int getResult;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentScene = SceneManager.GetActiveScene();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangeScene()
+    {
+        switch(currentScene.name)
+        {
+            case "Day 1":
+                DontDestroyOnLoad(gameObject);
+                if(getResult == 1)
+                {
+                    SceneManager.LoadScene("DAy 2A");
+                }
+                else if(getResult == 2)
+                {
+                    SceneManager.LoadScene("DAy 2B");
+                }
+                break;
+            case "Day 2A":
+                break;
+            case "Day 2B":
+                break;
+        }
+    }
+}
