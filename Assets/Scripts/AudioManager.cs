82d2f3652f423e7f0502dbf25f284801fed52a37
EmilStorm <90619317+EmilStorm@users.noreply.github.com>
2022-05-09 11:03:50 +0200

    AudioManager Script

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -1,14 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using FMOD.Studio;
+using FMODUnity;
 
 public class AudioManager : MonoBehaviour
 {
     public static AudioManager instance;
+    public EventInstance mainBGM;
+    public float deck;
+    public float night;
+    public float day2;
+    public PLAYBACK_STATE pbsBGM;
+    public Scene scene;
 
     private void Awake()
     {
         ManageSingleton();
+        scene = SceneManager.GetActiveScene();
+        mainBGM = RuntimeManager.CreateInstance("event:/bgm/main");
+        deck = 1f;
+        night = 0f;
+        day2 = 0f;
+        mainBGM.setParameterByName("deck", deck);
+        mainBGM.setParameterByName("night", night);
+        mainBGM.setParameterByName("day2", day2);
     }
 
     void ManageSingleton()
@@ -23,5 +40,35 @@ public class AudioManager : MonoBehaviour
             instance = this;
             DontDestroyOnLoad(gameObject);
         }
+    }
+    public void FMODStartBGM()
+    {
+        mainBGM.start();
+    }
+
+    public void FMODToggleTOD()
+    {
+        mainBGM.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+        if (night == 0)
+            night = 1f;
+        else
+            night = 0f;
+        mainBGM.start();
+    }
+
+    public void FMODToggleDeck()
+    {
+        if (deck == 0)
+            deck = 1f;
+        else
+            deck = 0f;
+    }
+
+    public void FMODDayChecker()
+    {
+        if (scene.name == "Day 1")
+            day2 = 0f;
+        else if (scene.name == "Day 2A" || scene.name == "Day 2B")
+            day2 = 1f;
     }
 }
