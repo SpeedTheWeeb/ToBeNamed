f5a8ccb0a40a50e2aab7b12052323a8f7016eb2b
Johnny Nguyen <nguyenjohnny655@gmail.com>
2022-05-09 11:49:06 +0200

    player and ship gets saved over scene

--- Assets/Scripts/Objects/SceneChanger.cs
+++ Assets/Scripts/Objects/SceneChanger.cs
@@ -7,6 +7,8 @@ public class SceneChanger : MonoBehaviour
     Scene currentScene;
     public GameObject robe;
     public int getResult = 0;
+    public GameObject ship;
+    public GameObject player;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,10 +23,13 @@ public class SceneChanger : MonoBehaviour
 
     public void ChangeScene()
     {
-        switch(currentScene.name)
+        DontDestroyOnLoad(gameObject);
+        DontDestroyOnLoad(player);
+        DontDestroyOnLoad(ship);
+        switch (currentScene.name)
         {
             case "Day 1":
-                DontDestroyOnLoad(gameObject);
+                
                 if(getResult != 0)
                 {
                     SceneManager.LoadScene("Day 2A");
