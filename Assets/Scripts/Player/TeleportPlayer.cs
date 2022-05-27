0bb2879eee51e8a03c3bd67165389ac162f4324a
EmilStorm <emilstorm@stormkofod.dk>
2022-05-27 09:33:21 +0200

    FMOD: added door sfx

--- Assets/Scripts/Player/TeleportPlayer.cs
+++ Assets/Scripts/Player/TeleportPlayer.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using FMODUnity;
+using FMOD.Studio;
 
 public class TeleportPlayer : MonoBehaviour
 {
     public GameObject tpPoint;
     public GameObject Player;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,6 +24,7 @@ public class TeleportPlayer : MonoBehaviour
         if(other.name == "TPObj")
         {
             Debug.Log(tpPoint.transform.position);
+            RuntimeManager.PlayOneShot("event:/sfx/oneshot/open_door");
             Player.transform.position = tpPoint.transform.position;
             if(tpPoint.name == "UnderUpperTP" || tpPoint.name == "UpperTP")
                 FindObjectOfType<AudioManager>().FMODToggleDeck();
