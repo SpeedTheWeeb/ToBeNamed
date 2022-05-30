ab855100258cd9ff3d42e91cbb32b4e7fddc6fa0
EmilStorm <emilstorm@stormkofod.dk>
2022-05-30 10:45:03 +0200

    Fixed Sound Functionality for epilogue and Main Menu

--- Assets/Scripts/UI/EpilogueButton.cs
+++ Assets/Scripts/UI/EpilogueButton.cs
@@ -2,11 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using FMOD.Studio;
+using FMODUnity;
 
 public class EpilogueButton : MonoBehaviour
 {
+    AudioManager audioManager;
+    private void Start()
+    {
+        audioManager = FindObjectOfType<AudioManager>();
+    }
     public void QuitToMainMenu()
     {
+        audioManager.StopMusic();
         SceneManager.LoadScene("mainMenu");
     }
 }
