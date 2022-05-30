6871574be8111a00a6afc24751fcedecab3d6db3
SpeedTheWeeb <nguyenjohnny655@gmail.com>
2022-05-30 11:04:45 +0200

    Armory door open and added key

---
+++ Assets/Scripts/Day 1 Scripts/OpenDoor.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoor : MonoBehaviour
+{
+    public bool isInteractable;
+    GameObject interactedObj;
+    Vector3 rotateVector = new Vector3(0, 0, 0);
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Interact") && isInteractable)
+        {
+            transform.rotation = Quaternion.Euler(rotateVector);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Object") && other.name == "Key")
+        {
+            interactedObj = other.gameObject;
+            isInteractable = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        interactedObj = null;
+        isInteractable = false;
+    }
+}
